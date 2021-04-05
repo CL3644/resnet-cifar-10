f69ca92d72eb272c0d52b466f87201e02151cb04
Calvin Lu <calvinlu@Joshs-MBP.fios-router.home>
2021-04-04 21:12:08 -0400

    edits

--- train_res50.py
+++ train_res50.py
@@ -9,9 +9,6 @@ import torch.optim as optim
 import time
 import pandas as pd
 
-from torch.utils.tensorboard import SummaryWriter
-writer = SummaryWriter()
-
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
 
